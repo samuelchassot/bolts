5346d55d4e27504b02dac17564ad87695b610a00
Samuel Chassot <samuel.chassot@epfl.ch>
2023-10-23 12:13:25 +0200

    write code to show that our map does not trigger the bug in the original

--- data-structures/longmap/bug_in_original/BugLongMapReal.scala
+++ data-structures/longmap/bug_in_original/BugLongMapReal.scala
@@ -1,7 +1,9 @@
 import scala.collection.mutable.LongMap
+import ch.epfl.chassot.MutableLongMap
 
 // To run it using scala-cli with enough heap space:
-// scala-cli -J -Xmx24576m BugLongMap.scala
+// scala-cli -J -Xmx24576m BugLongMapReal.scala ../MutableLongMap.scala ../StrictlyOrderedLongListMap.scala $(find /localhome/chassot/stainless/frontends/library/stainless/ -name "*.scala")
+
 /** This makes the LongMap hangs, when the size reaches 268'435'456 So the counter example we found in Stainless is actually triggering a bug :)
   */
 object BugLongMap {
@@ -13,8 +15,16 @@ object BugLongMap {
       println(f"m.size = ${m.size} for i = $i")
     }
   }
+  def notTriggerBugVerified(): Unit = {
+    val m = MutableLongMap.getEmptyLongMap(l => -1L, 16)
+    for (i <- 0 to Math.pow(2, 30).toInt) {
+      m.update(i, i)
+      println(f"m.size = ${m.size} for i = $i")
+    }
+  }
 }
 
 @main def main(): Unit = {
-  BugLongMap.maintriggerBugealClass()
+  // BugLongMap.triggerBug()
+  BugLongMap.notTriggerBugVerified()
 }
